ba9bf55689ab7ce629f7801871524f35b63da02c
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-09-04 04:13:40 -0700

    Apollo Server settings

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,10 +1,25 @@
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { ApolloServer } from '@apollo/server';
+import {
+  ApolloServerPluginLandingPageProductionDefault,
+  ApolloServerPluginLandingPageLocalDefault,
+} from '@apollo/server/plugin/landingPage/default';
 import schema from '@/lib/graphql/schema';
 import type { NextRequest } from 'next/server';
 
 const server = new ApolloServer({
   schema,
+  introspection: true,
+  plugins: [
+    // Install a landing page plugin based on NODE_ENV
+    process.env.NODE_ENV === 'production'
+      ? ApolloServerPluginLandingPageProductionDefault({
+          graphRef: 'my-graph-id@my-graph-variant',
+          embed: true,
+          footer: false,
+        })
+      : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+  ],
 });
 
 const handler = startServerAndCreateNextHandler<NextRequest>(server, {
