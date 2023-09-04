eb5ad4366c087f46ab714f2ad57388bacbaf9377
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-09-04 04:44:48 -0700

    OpenGraph metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,20 @@ export const metadata: Metadata = {
     { media: '(prefers-color-scheme: light)', color: '#ffffff' },
     { media: '(prefers-color-scheme: dark)', color: '#191719' },
   ],
+  openGraph: {
+    title: 'Slime Rancher GraphQL',
+    description: 'A GraphQL endpoint for Slime Rancer',
+    url: 'https://slime-rancher-graphql.vercel.app/',
+    images: [
+      {
+        url: 'https://slime-rancher-graphql.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fslimes.bf78d2a6.webp&w=640&q=75',
+        width: 512,
+        height: 300,
+      }
+    ],
+    locale: 'en_CA',
+    type: 'website',
+  },
   twitter: {
     card: 'summary_large_image',
     title: 'Slime Rancher GraphQL',
