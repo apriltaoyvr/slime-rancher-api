d01fbbb814d6ac8685e97947eb050de3a66bb807
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-09-07 20:04:17 -0700

    Remove unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import '@radix-ui/themes/styles.css';
 import './globals.css';
 import Providers from './providers';
 import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
