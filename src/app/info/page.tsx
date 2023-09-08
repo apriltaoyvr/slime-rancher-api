85ea65bafba6e159ee70146932ac1a6bb661f441
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-09-07 20:56:00 -0700

    Add callout

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -1,11 +1,6 @@
-import {
-  Container,
-  Section,
-  Flex,
-  Heading,
-  Text,
-} from '@radix-ui/themes';
+import { Container, Section, Flex, Heading, Text } from '@radix-ui/themes';
 import Link from 'next/link';
+import CalloutComponent from '@/components/CalloutComponent';
 
 // Sections
 import GqlSection from './(sections)/GqlSection';
@@ -14,16 +9,19 @@ import ApiSection from './(sections)/ApiSection';
 export default function Info() {
   return (
     <Container>
-      <Flex direction='column' align='center' m='2' gap='2'>
+      <Flex direction='column' align='center' m='2' gap='4'>
         <Heading align='center' size='8' as='h1' mb='2'>
           Info
         </Heading>
+        <CalloutComponent>
+          Slime Rancher 2 information is currently being added.
+        </CalloutComponent>
         <Text>
           This API offers both RESTful and GraphQL endpoints. Let's ranch! 🌟🐌
         </Text>
       </Flex>
-      <GqlSection/>
-      <ApiSection/>
+      <GqlSection />
+      <ApiSection />
       <Section id='credits' size='1'>
         <Flex direction='column' align='center'>
           <Heading align='center' as='h2' m='2'>
