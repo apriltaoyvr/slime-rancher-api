802ac33439bd328fcebc42f5082b898c39a9480e
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-09-11 14:43:06 -0700

    Fetch from localhost in development

--- src/lib/gqlFetcher.ts
+++ src/lib/gqlFetcher.ts
@@ -1,8 +1,18 @@
+const endpoint =
+  process.env.NODE_ENV === 'production'
+    ? 'https://slime-rancher.vercel.app/graphql'
+    : 'http://localhost:3000/graphql';
+
 export default async function graphqlFetcher(
   query: string,
-  args: { slimeId?: string; foodId?: string; locationId?: string } = {},
+  args: {
+    slimeId?: string;
+    foodId?: string;
+    locationId?: string;
+    toyId?: string;
+  } = {},
 ) {
-  const res = await fetch('https://slime-rancher.vercel.app/graphql', {
+  const res = await fetch(endpoint, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
