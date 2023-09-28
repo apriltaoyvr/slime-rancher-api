40cd5b61485f0ed5a9eec56787dcb3bf3225afc9
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-09-28 02:13:49 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    removeConsole: {
+      exclude: ['error'],
+    },
+  },
   images: {
     remotePatterns: [
       {
