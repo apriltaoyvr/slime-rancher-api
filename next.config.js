e7886e32ba2733e61aa9284fe0222486c5640af3
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-09-28 02:37:23 -0700

    Add wikia as a remote image host

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'raw.githubusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'static.wikia.nocookie.net',
+      },
     ],
   },
 };
